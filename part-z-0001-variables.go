76035934ac7face0acd0f26afda9609d27ec2b6e
Mustafa Buyukdereli <77589867+mustafaontheway@users.noreply.github.com>
2025-12-17 08:55:06 +0300

    Create part-z-0001-variables.go

---
+++ part-z-0001-variables.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	var year uint16 = 2025
+
+	var who = "Mustafa"
+
+	var aNumber = 11
+
+	country := "Canada"
+
+	fmt.Println(year, who, aNumber, country)
+ }
