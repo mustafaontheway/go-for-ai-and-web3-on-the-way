f6f98b6b2fa27a31988f664e5b56456482f8ebc2
Mustafa Buyukdereli <77589867+mustafaontheway@users.noreply.github.com>
2025-12-17 09:18:02 +0300

    Create part-z-0004-floats.go

---
+++ part-z-0004-floats.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	var num1 float32 = 22.0 / 7
+
+	var num2 float64 = 22.0 / 7
+
+	fmt.Println(num1)
+	fmt.Println(num2)
+ }
+
+// 3.142857
+// 3.142857142857143
