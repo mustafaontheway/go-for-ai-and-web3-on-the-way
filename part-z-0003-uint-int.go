cdcc3bfebf71f0e85fd5f3ddab4e8d1c9ca0815c
Mustafa Buyukdereli <77589867+mustafaontheway@users.noreply.github.com>
2025-12-17 09:14:26 +0300

    Create part-z-0003-uint-int.go

---
+++ part-z-0003-uint-int.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	var x = 11
+
+	var y uint = 100
+
+	var z int = -500_000
+
+	var age uint8 = 99
+
+	var balance int16 = 2500
+
+	var totalSales uint64 = 654_421_321
+
+	fmt.Println(x, y, z, age, balance, totalSales)
+ }
