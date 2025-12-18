431f5efffb7a0499307a3caf44bb579ff94c7dc0
Mustafa Buyukdereli <77589867+mustafaontheway@users.noreply.github.com>
2025-12-18 20:41:02 +0300

    Create part-z-0005-type-casting1.go

---
+++ part-z-0005-type-casting1.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	var num1 float32 = 22.0 / 7
+
+	num2 := uint8(num1)
+
+	num3 := int(num1)
+
+	fmt.Println(num1)
+	fmt.Println(num2)
+	fmt.Println(num3)
+ }
+
+// 3.142857
+// 3
+// 3
