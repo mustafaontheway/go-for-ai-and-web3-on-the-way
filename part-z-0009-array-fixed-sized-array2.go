d42df212cd97a92bd01d3a59c46ca0a3e30f4c6b
Mustafa Buyukdereli <77589867+mustafaontheway@users.noreply.github.com>
2025-12-22 11:09:25 +0300

    Create part-z-0009-array-fixed-sized-array2.go

---
+++ part-z-0009-array-fixed-sized-array2.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	var ages = [...]uint8{12, 9, 99, 41, 21, 77, 96, 81, 40}
+
+	fmt.Println(ages)
+
+	fmt.Println(ages[4])
+	
+}
+
+// [12 9 99 41 21 77 96 81 40]
+// 21
