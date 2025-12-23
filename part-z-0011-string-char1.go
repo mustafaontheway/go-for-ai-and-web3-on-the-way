16b9573f810907e679ab809334d42f1c58877821
Mustafa Buyukdereli <77589867+mustafaontheway@users.noreply.github.com>
2025-12-23 20:06:10 +0300

    Create part-z-0011-string-char1.go

---
+++ part-z-0011-string-char1.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	var city = "Ankara"
+
+	// https://www.ascii-code.com/characters/A-Z
+
+	fmt.Println("First char:", city[0]) // First char: 65
+
+	fmt.Printf("First char: %c", city[0]) //First char: A
+}
+
