86590b0c2b43945471d208d35292975794e2d87c
Mustafa Buyukdereli <77589867+mustafaontheway@users.noreply.github.com>
2026-01-08 19:39:45 +0300

    Create part-z-0035-module-package3-main.go

---
+++ part-z-0035-module-package3-main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+	"mathoperations/mathfuncs" //module_name+package_name
+) 
+
+func main() {
+
+	fmt.Println(mathfuncs.MinusFive(3))
+	fmt.Println(mathfuncs.MinusTen(3))
+	fmt.Println(mathfuncs.PlusFive(3))
+	fmt.Println(mathfuncs.PlusTen(3))
+}
+
+// -2
+// -7
+// 8
+// 13
