4ae1cb59a9be5ae78e36b1fe932cc22e07fc65cb
Mustafa Buyukdereli <77589867+mustafaontheway@users.noreply.github.com>
2026-01-11 21:42:07 +0300

    Create part-z-0036-for-loop-while-simulation2.go

---
+++ part-z-0036-for-loop-while-simulation2.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+) 
+
+func main() {
+
+	var counter uint8 = 10 
+
+	/*
+	we can use => for {
+		fmt.Println("Infinite loop is working...")
+	} 
+	*/
+
+	for counter > 0 {
+
+		fmt.Println("Infinite loop is working...")
+	}
+}
+
+
+
